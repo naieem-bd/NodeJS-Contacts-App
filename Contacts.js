3b0ee9edc6d3a7ecbf1fa930317f9c1937ec0141
naieem-bd <naieemur@gmail.com>
2021-02-13 18:18:47 +0600

    modified Contacts class

--- Contacts.js
+++ Contacts.js
@@ -16,4 +16,23 @@ class Contacts {
         this.contacts.push(contact)
         return contact
     }
+
+    updateContactById(id, updatedContact) {
+        let index = this.contacts.findIndex(contact => contact.id === id)
+
+        this.contacts[index].name = updatedContact.name || this.contacts[index].name
+        this.contacts[index].phone = updatedContact.phone || this.contacts[index].phone
+        this.contacts[index].email = updatedContact.email || this.contacts[index].email
+
+        return this.contacts[index]
+    }
+
+    deleteContactById(id) {
+        let index = this.contacts.findIndex(contact => contact.id === id)
+        let deletedObj = this.contacts[index]
+        this.contacts = this.contacts.filter(contact => contact.id !== id)
+        return deletedObj
+    }
 }
+
+exports.contacts = new Contacts()
