2336caea30b09aa56517868cb768850bc99c23b4
naieem-bd <naieemur@gmail.com>
2021-02-14 18:01:38 +0600

    modified | router added

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+
+const contactRoutes = require('./contactRoutes')
 
 const app = express()
+app.use(morgan('dev'))
+
+app.use('/contacts', contactRoutes)
+
+app.get('*', (req, res) => {
+    res.send('<p>please use the correct routes</p>')
+})
 
 const PORT = process.env.PORT || 2727
 app.listen(PORT, () => {
